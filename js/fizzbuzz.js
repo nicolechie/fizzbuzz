19a88663d5a8608d20495c7b88fe85c7b035463c
nicolechie <nicolechie@mac.com>
2016-02-12 16:29:43 -0700

    printed to screen

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,17 +1,19 @@
 $(document)
 	.ready(function() {
 	for (var i = 1; i < 101; i++) {
+		var icopy;
 		if (i%3 === 0 && i%5 !== 0) {
-			console.log("Fizz");
+			icopy = "Fizz";
 		}
 		else if (i%5 === 0 && i%3 !== 0) {
-			console.log("Buzz");
+			icopy = "Buzz";
 		}
-		else if (i%3 === 0 & i%5 === 0) {
-			console.log("FizzBuzz");
+		else if (i%3 === 0 && i%5 === 0) {
+			icopy = "FizzBuzz";
 		}
 		else {
-			console.log(i);
+			icopy = i;
 		}
+		$('ul').append('<li>'+ icopy +'</li>');
 	}
 });
