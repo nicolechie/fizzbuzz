71493fdbf994143e52d8fca5ef0801141a66aa02
nicolechie <nicolechie@mac.com>
2016-02-12 18:37:25 -0700

    prompt added'

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,6 +1,7 @@
 $(document)
 	.ready(function() {
-	for (var i = 1; i < 101; i++) {
+		var answer = prompt('Input a number');
+	for (var i = 1; i <= answer; i++) {
 		var icopy;
 		if (i%3 === 0 && i%5 !== 0) {
 			icopy = "Fizz";
